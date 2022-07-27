818fee19fae0ca4290560dcc63c59cb5e558c5f4
Timothy RYAN <ryaninlux@gmail.com>
2022-07-26 20:49:30 -0700

    Add controllers and cshtml

--- Restaurant/Controllers/CuisinesController.cs
+++ Restaurant/Controllers/CuisinesController.cs
@@ -29,5 +29,50 @@ namespace Restaurant.Controllers
       ViewBag.CuisineId = new SelectList(_db.Cuisines, "CuisineId", "CuisineType");
       return View();
     }
+
+    [HttpPost]
+    public ActionResult Create(Cuisine cuisine)
+    {
+      _db.Cuisines.Add(cuisine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Cuisine thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+      return View(thisCuisine);
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+      return View(thisCuisine);
+    }
+    //POST action is named DeleteConfirmed instead of Delete since both GET + POST action methods take id as a parameter. C# will not allow us to have two methods with the same signature(method name and parameters). The POST attribute is not considered part of the method signature 
+
+    [HttpPost, ActionName("Delete")] //Note that our annotation includes [ActionName("Delete")]. This is so we can still utilize the proper Delete action even though we've named our method DeleteConfirmed.
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+      _db.Cuisines.Remove(thisCuisine);//built in Remove method
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    //GET action routes to page with a form for updating item
+    public ActionResult Edit(int id)
+    {
+      Cuisine thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+      ViewBag.CuisineId = new SelectList(_db.Cuisines, "CuisineId", "CuisineType");
+      return View(thisCuisine);
+    }//finding specific eatery and passing it into view
+
+    [HttpPost]//POST actually updates item
+    public ActionResult Edit(Cuisine cuisine)
+    {
+      _db.Entry(cuisine).State = EntityState.Modified;//We find and update all of the properties of the item we are editing by passing the item (our route parameter) itself into the Entry() method. Then we need to update its State property to EntityState.Modified. This is so Entity knows that the entry has been modified, as it is not explicitly tracking it (we never actually retrieved the item from the database).
+      _db.SaveChanges();//once entry's state is marked as modified we can save and then redirect to Index view
+      return RedirectToAction("Index");
+    }
   }
 }
